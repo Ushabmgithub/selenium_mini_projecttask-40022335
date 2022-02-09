6504c2a84e87e15d74ba6f5cee6f821d05748e7b
Ushabmgithub <Usha.BM@Ltts.com>
2022-02-09 22:45:58 +0530

    selenium

---
+++ Task2.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.keys import Keys
+import time
+browser = webdriver.Firefox(executable_path = "C:\geckodriver-v0.30.0-win64\geckodriver")
+browser.get('http://automationpractice.com/index.php')
+browser.find_element_by_class_name("login").send_keys(Keys.ENTER)
+time.sleep(5)
+browser.find_element_by_id("email_create").send_keys("usha@gmail@.com")
+browser.find_element_by_name("SubmitCreate").send_keys(Keys.ENTER)
+time.sleep(5)
